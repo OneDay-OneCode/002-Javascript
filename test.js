628975c285751b85bd141118b3fc4470196297c6
MarkBurnsCode <121692996+MarkBurnsCode@users.noreply.github.com>
2023-01-02 19:48:37 +0100

    Operations and incrementals
    .

---
+++ test.js
@@ -0,0 +1,37 @@
+/* --------------------------- NUMBERS AND OPERATIONS ------------------------------- */
+
+var num1 = 10;
+var num2 = 5;
+
+var suma = num1 + num2;
+var resta = num1 - num2;
+var multiplicacion = num1 * num2;
+var division = num1 / num2;
+var decimal = num2 / num1;
+
+console.log(suma);
+console.log(resta);
+console.log(multiplicacion);
+console.log(division);
+console.log(decimal);
+
+
+/* --------------------------- NUMBERS AND INCREMENTALS ------------------------------- */
+
+
+var a = 100;
+var b = a;
+var c = a;
+var d = a;
+
+a+=10;
+b-=10;
+c*=10;
+d/=10;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+
+/* ----------------------------------------------------------- */
